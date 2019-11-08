554712b766a168c67d74f16464a91ff7e23efc98
Aoife <c00218374@itcarlow.ie>
2019-11-08 16:34:38 +0000

    updated

---
+++ Bubble Component/Assets/Scripts/BubbleMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleMovement : MonoBehaviour
+{
+    public int destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= destination)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        //particles;
+    }
+}
