b5711d7ad3bdf349c279413f49b9e44ce2d044c3
Aoife Powders <c00218374@itcarlow.ie>
2020-01-22 23:55:33 +0000

    Test]

---
+++ Bubble Component/Assets/Test Scripts/MovementFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementFilter
+{
+    public static bool getBubble()
+    {
+        if (GameObject.Find("Bubble") != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static Vector3 getPosition()
+    {
+        GameObject bubble = GameObject.FindGameObjectWithTag("bubble");
+        return bubble.transform.position;
+    }
+}
---
+++ Bubble Component/Assets/Tests/MovementTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MovementTest
+    {
+        GameObject bubble;
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MovementTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            bool output = MovementFilter.getBubble();
+
+            Assert.AreEqual(false, output);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MovementTestWithEnumeratorPasses()
+        {
+            bubble = GameObject.FindGameObjectWithTag("bubble");
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            Vector3 output = bubble.transform.position;
+            bool outputbool = MovementFilter.getBubble();
+            yield return new WaitForSeconds(5.0f);
+
+            Assert.AreNotEqual(MovementFilter.getPosition(), output);
+        }
+    }
+}
